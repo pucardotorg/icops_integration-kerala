6def8733a2e7bdbcec5c8fa1ad231c448400d18e
Ajithjaya <ajith30022@gmail.com>
2024-05-09 19:11:47 +0530

    maven folder and test file

---
+++ src/test/java/com/egov/icops_integrationkerala/IcopsIntegrationKeralaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egov.icops_integrationkerala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcopsIntegrationKeralaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
