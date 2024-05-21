8e163f0423f10db741bf3032efafd6c7d61c8dc1
Ajithjaya <ajith30022@gmail.com>
2024-05-21 11:26:53 +0530

    jwt util creating secure secret key

--- src/main/java/com/egov/icops_integrationkerala/util/JwtUtil.java
+++ src/main/java/com/egov/icops_integrationkerala/util/JwtUtil.java
@@ -2,15 +2,17 @@ package com.egov.icops_integrationkerala.util;
 
 import com.egov.icops_integrationkerala.model.AuthToken;
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 @Component
 public class JwtUtil {
 
-    private final String secretKey = "mySecretKey";
+    private final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     private final String TOKEN_HEADER = "Authorization";
 
@@ -26,11 +28,12 @@ public class JwtUtil {
 
     public AuthToken generateToken(String serviceName) {
         Date tokenCreateTime = new Date();
-        Date tokenValidity = new Date(tokenCreateTime.getTime() + TimeUnit.MINUTES.toMillis(tokenExpirationTime));
+        Date tokenExpiryTime = new Date(tokenCreateTime.getTime() + TimeUnit.MINUTES.toMillis(tokenExpirationTime));
         String accessToken =  Jwts.builder()
                 .setSubject(serviceName)
-                .setExpiration(tokenValidity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .setIssuedAt(tokenCreateTime)
+                .setExpiration(tokenExpiryTime)
+                .signWith(secretKey)
                 .compact();
         return AuthToken.builder()
                 .accessToken(accessToken).expiresIn((int) tokenExpirationTime)
