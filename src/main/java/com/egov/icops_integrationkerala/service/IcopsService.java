d42918738368d22059e5c8edc72cb287e7f21ff6
Ajith-beehyv <ajith.k@beehyv.com>
2024-06-19 18:28:43 +0530

    Process Request Util

--- src/main/java/com/egov/icops_integrationkerala/service/IcopsService.java
+++ src/main/java/com/egov/icops_integrationkerala/service/IcopsService.java
@@ -7,19 +7,16 @@ import com.egov.icops_integrationkerala.model.*;
 import com.egov.icops_integrationkerala.util.AuthUtil;
 import com.egov.icops_integrationkerala.util.FileStorageUtil;
 import com.egov.icops_integrationkerala.util.JwtUtil;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.egov.icops_integrationkerala.util.ProcessRequestUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestClientException;
 
-import java.time.LocalDate;
 import java.util.UUID;
 
 @Service
@@ -43,10 +40,12 @@ public class IcopsService {
 
     private final IcopsEnrichment icopsEnrichment;
 
+    private final ProcessRequestUtil processRequestUtil;
+
     @Autowired
     public IcopsService(MyRestTemplateConfig restTemplate, ObjectMapper objectMapper,
                         IcopsConfiguration config, AuthUtil authUtil,
-                        AuthenticationManager authenticationManager, JwtUtil jwtUtil, FileStorageUtil fileStorageUtil, IcopsEnrichment icopsEnrichment) {
+                        AuthenticationManager authenticationManager, JwtUtil jwtUtil, FileStorageUtil fileStorageUtil, IcopsEnrichment icopsEnrichment, ProcessRequestUtil processRequestUtil) {
         this.restTemplate = restTemplate;
         this.objectMapper = objectMapper;
         this.config = config;
@@ -55,35 +54,14 @@ public class IcopsService {
         this.jwtUtil = jwtUtil;
         this.fileStorageUtil = fileStorageUtil;
         this.icopsEnrichment = icopsEnrichment;
+        this.processRequestUtil = processRequestUtil;
     }
 
 
     public ChannelMessage sendRequestToIcops(SendSummonsRequest summonsRequest) throws Exception {
         ProcessRequest processRequest = getProcessRequest(summonsRequest.getTaskSummon());
         AuthToken authResponse = authUtil.authenticateAndGetToken();
-        String icopsUrl = config.getIcopsUrl() + config.getProcessRequestEndPoint();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization", "Bearer " + authResponse.getAccessToken());
-        HttpEntity<ProcessRequest> requestEntity = new HttpEntity<>(processRequest, headers);
-        try {
-            log.info("Request Body: " + objectMapper.writeValueAsString(processRequest));
-            // Send the request and get the response
-            ResponseEntity<Object> responseEntity =
-                    restTemplate.restTemplate().postForEntity(icopsUrl, requestEntity, Object.class);
-            ChannelMessage response = objectMapper.convertValue(responseEntity.getBody(), ChannelMessage.class);
-            // Print the response body and status code
-            log.info("Response Body: " + objectMapper.writeValueAsString(responseEntity.getBody()));
-            log.info("Status Code: " + responseEntity.getStatusCode());
-            return response;
-        } catch (RestClientException e) {
-            log.error("Error occurred when sending Process Request ", e);
-            log.error(e.getMessage());
-            throw new Exception("Error occurred when sending Process Request");
-        } catch (JsonProcessingException e) {
-            log.error("Error occurred when logging Process response ", e);
-            throw new Exception("Error occurred when logging Process response");
-        }
+        return processRequestUtil.callProcessRequest(authResponse, processRequest);
     }
 
     private ProcessRequest getProcessRequest(TaskSummon taskSummon) {
---
+++ src/main/java/com/egov/icops_integrationkerala/util/ProcessRequestUtil.java
@@ -0,0 +1,59 @@
+package com.egov.icops_integrationkerala.util;
+
+import com.egov.icops_integrationkerala.config.IcopsConfiguration;
+import com.egov.icops_integrationkerala.model.AuthToken;
+import com.egov.icops_integrationkerala.model.ChannelMessage;
+import com.egov.icops_integrationkerala.model.ProcessRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+@Slf4j
+public class ProcessRequestUtil {
+
+    private final RestTemplate restTemplate;
+
+    private final IcopsConfiguration config;
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public ProcessRequestUtil(RestTemplate restTemplate, IcopsConfiguration config, ObjectMapper objectMapper) {
+        this.restTemplate = restTemplate;
+        this.config = config;
+        this.objectMapper = objectMapper;
+    }
+
+    public ChannelMessage callProcessRequest(AuthToken authResponse, ProcessRequest processRequest) throws Exception {
+        String icopsUrl = config.getIcopsUrl() + config.getProcessRequestEndPoint();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Authorization", "Bearer " + authResponse.getAccessToken());
+        HttpEntity<ProcessRequest> requestEntity = new HttpEntity<>(processRequest, headers);
+        try {
+            log.info("Request Body: " + objectMapper.writeValueAsString(processRequest));
+            // Send the request and get the response
+            ResponseEntity<ChannelMessage> responseEntity =
+                    restTemplate.postForEntity(icopsUrl, requestEntity, ChannelMessage.class);
+            // Print the response body and status code
+            log.info("Response Body: " + objectMapper.writeValueAsString(responseEntity.getBody()));
+            return responseEntity.getBody();
+        } catch (RestClientException e) {
+            log.error("Error occurred when sending Process Request ", e);
+            log.error(e.getMessage());
+            throw new Exception("Error occurred when sending Process Request");
+        } catch (JsonProcessingException e) {
+            log.error("Error occurred when logging Process response ", e);
+            throw new Exception("Error occurred when logging Process response");
+        }
+    }
+}
