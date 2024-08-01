bfb605ec60a705d25ee5d3df1b0500a008f31ea5
harshkumar8789 <harsh.kumar@beehyv.com>
2024-08-01 11:28:07 +0530

    update producer

--- src/main/java/com/egov/icops_integrationkerala/kafka/Producer.java
+++ src/main/java/com/egov/icops_integrationkerala/kafka/Producer.java
@@ -1,11 +1,10 @@
 package com.egov.icops_integrationkerala.kafka;
 
 
-// NOTE: If tracer is disabled change CustomKafkaTemplate to KafkaTemplate in autowiring
 
 import lombok.extern.slf4j.Slf4j;
-import org.egov.tracer.kafka.CustomKafkaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +12,7 @@ import org.springframework.stereotype.Service;
 public class Producer {
 
     @Autowired
-    private CustomKafkaTemplate<String, Object> kafkaTemplate;
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
     public void push(String topic, Object value) {
         kafkaTemplate.send(topic, value);
