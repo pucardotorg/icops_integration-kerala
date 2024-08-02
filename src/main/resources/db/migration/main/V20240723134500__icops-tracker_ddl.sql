608fcd6ae384ef13d113df40b74f7a8419685148
harshkumar8789 <harsh.kumar@beehyv.com>
2024-08-02 13:49:21 +0530

    change table.

--- src/main/resources/db/migration/main/V20240723134500__icops-tracker_ddl.sql
+++ src/main/resources/db/migration/main/V20240723134500__icops-tracker_ddl.sql
@@ -7,11 +7,12 @@ CREATE TABLE dristi_kerala_icops (
     file_store_id varchar(64) NULL,
     task_details jsonb NULL,
     delivery_status varchar(64) NULL,
-    acknowledgement_id varchar(64) NULL,
     remarks varchar(64) NULL,
     additional_details jsonb NULL,
+    row_version int4 NULL,
+    acknowledgement_id varchar(64) NULL,
     booking_date varchar(64) NULL,
     received_date varchar(64) NULL,
-    row_version int4 NULL
+
 );
  
