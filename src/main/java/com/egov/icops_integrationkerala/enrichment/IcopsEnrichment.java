933bf7f362db46fa91aeabeee015970bbb27a2b8
harshkumar8789 <harsh.kumar@beehyv.com>
2024-08-29 14:42:08 +0530

    icops fixes.

--- src/main/java/com/egov/icops_integrationkerala/enrichment/IcopsEnrichment.java
+++ src/main/java/com/egov/icops_integrationkerala/enrichment/IcopsEnrichment.java
@@ -64,10 +64,12 @@ public class IcopsEnrichment {
         if(!task.getTaskType().isEmpty() && task.getTaskType().equalsIgnoreCase("WARRANT")){
             String docSubType = Optional.ofNullable(taskDetails.getWarrantDetails().getDocSubType())
                     .orElse("Warrant of arrest");
+            Integer age = taskDetails.getRespondentDetails().getAge();
+            String ageString = (age != null) ? String.valueOf(age) : "";
 
             Map<String, String> docTypeInfo = getDocTypeCode(mdmsData, docSubType);
             PartyData partyData = PartyData.builder()
-                    .spartyAge(String.valueOf(taskDetails.getRespondentDetails().getAge()))
+                    .spartyAge(ageString)
                     .spartyName(taskDetails.getRespondentDetails().getName())
                     .spartyType("A")  // currently supported only for accused, so hardcoded it.
                     .spartyEmail(taskDetails.getRespondentDetails().getEmail())
@@ -104,6 +106,9 @@ public class IcopsEnrichment {
         else{
             String docSubType = Optional.ofNullable(taskDetails.getSummonDetails().getDocSubType())
                     .orElse("Summons to an accused person");
+            Integer age = taskDetails.getRespondentDetails().getAge();
+            String ageString = (age != null) ? String.valueOf(age) : "";
+
 
             Map<String, String> docTypeInfo = getDocTypeCode(mdmsData, docSubType);
              processRequest = ProcessRequest.builder()
@@ -116,7 +121,7 @@ public class IcopsEnrichment {
                     .processNextHearingDate(converter.convertLongToDate(taskDetails.getCaseDetails().getHearingDate()))
                     .processRespondentName(taskDetails.getRespondentDetails().getName())
                     .processRespondentGender(taskDetails.getRespondentDetails().getGender())
-                    .processRespondentAge(String.valueOf(taskDetails.getRespondentDetails().getAge()))
+                    .processRespondentAge(ageString)
                     .processRespondentRelativeName(taskDetails.getRespondentDetails().getRelativeName())
                     .processRespondentRelation(taskDetails.getRespondentDetails().getRelationWithRelative())
                     .processReceiverAddress(taskDetails.getRespondentDetails().getAddress().toString())
